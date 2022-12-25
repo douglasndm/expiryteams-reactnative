99784402d7bafd1c92b5ee68b7935ea310a71b7b
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-12-25 16:07:28 -0300

    About view is now shared

--- src/Components/DrawerMenu/UserInfo/index.tsx
+++ src/Components/DrawerMenu/UserInfo/index.tsx
@@ -1,75 +1,75 @@
 import React, { useCallback, useMemo } from 'react';
 
-import strings from '~/Locales';
+import strings from '@teams/Locales';
 
-import { useAuth } from '~/Contexts/AuthContext';
-import { useTeam } from '~/Contexts/TeamContext';
+import { useAuth } from '@teams/Contexts/AuthContext';
+import { useTeam } from '@teams/Contexts/TeamContext';
 
 import {
-    Container,
-    TextContainer,
-    UserName,
-    UserEmail,
-    UserInfo,
-    UserPhoto,
-    DefaultUserPhoto,
+	Container,
+	TextContainer,
+	UserName,
+	UserEmail,
+	UserInfo,
+	UserPhoto,
+	DefaultUserPhoto,
 } from './styles';
 
 interface InfoProps {
-    navigate: (route: string) => void;
+	navigate: (route: string) => void;
 }
 
 const Info: React.FC<InfoProps> = ({ navigate }: InfoProps) => {
-    const { user } = useAuth();
-    const teamContext = useTeam();
+	const { user } = useAuth();
+	const teamContext = useTeam();
 
-    const userRole = useMemo(() => {
-        if (teamContext.roleInTeam) {
-            const { role } = teamContext.roleInTeam;
+	const userRole = useMemo(() => {
+		if (teamContext.roleInTeam) {
+			const { role } = teamContext.roleInTeam;
 
-            if (role?.toLowerCase() === 'manager')
-                return strings.UserInfo_Role_Manager;
-            if (role?.toLowerCase() === 'supervisor') {
-                return strings.UserInfo_Role_Supervisor;
-            }
-        }
+			if (role?.toLowerCase() === 'manager')
+				return strings.UserInfo_Role_Manager;
+			if (role?.toLowerCase() === 'supervisor') {
+				return strings.UserInfo_Role_Supervisor;
+			}
+		}
 
-        return strings.UserInfo_Role_Repositor;
-    }, [teamContext.roleInTeam]);
+		return strings.UserInfo_Role_Repositor;
+	}, [teamContext.roleInTeam]);
 
-    const handleNavigateToProfile = useCallback(() => {
-        navigate('User');
-    }, [navigate]);
+	const handleNavigateToProfile = useCallback(() => {
+		navigate('User');
+	}, [navigate]);
 
-    return (
-        <Container onPress={handleNavigateToProfile}>
-            {user && (
-                <>
-                    {user?.photoURL ? (
-                        <UserPhoto source={{ uri: user?.photoURL }} />
-                    ) : (
-                        <DefaultUserPhoto />
-                    )}
+	return (
+		<Container onPress={handleNavigateToProfile}>
+			{user && (
+				<>
+					{user?.photoURL ? (
+						<UserPhoto source={{ uri: user?.photoURL }} />
+					) : (
+						<DefaultUserPhoto />
+					)}
 
-                    <TextContainer>
-                        {!!user.displayName && (
-                            <UserName>{user.displayName}</UserName>
-                        )}
+					<TextContainer>
+						{!!user.displayName && (
+							<UserName>{user.displayName}</UserName>
+						)}
 
-                        {!!user.email && <UserEmail>{user?.email}</UserEmail>}
+						{!!user.email && <UserEmail>{user?.email}</UserEmail>}
 
-                        {!!teamContext.name && (
-                            <UserInfo>
-                                {`${
-                                    teamContext.name
-                                } (${userRole.toUpperCase()})`}
-                            </UserInfo>
-                        )}
-                    </TextContainer>
-                </>
-            )}
-        </Container>
-    );
+						{!!teamContext.name && (
+							<UserInfo>
+								{`${
+									teamContext.name
+								} (${userRole.toUpperCase()})`}
+							</UserInfo>
+						)}
+					</TextContainer>
+				</>
+			)}
+		</Container>
+	);
 };
 
 export default Info;
