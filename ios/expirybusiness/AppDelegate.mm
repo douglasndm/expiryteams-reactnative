6ac7774fc962ae227fa81b31acced4e0ce0ccb0f
Douglas Nunes de Mattos <7841509+douglasndm@users.noreply.github.com>
2023-01-19 22:12:24 -0300

    Updating to RN 0.71.1

--- ios/expirybusiness/AppDelegate.mm
+++ ios/expirybusiness/AppDelegate.mm
@@ -13,6 +13,9 @@
     [FIRApp configure];
 
     self.moduleName = @"expirybusiness";
+    // You can add your custom initial props in the dictionary below.
+    // They will be passed down to the ViewController used by React Native.
+    self.initialProps = @{};
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
