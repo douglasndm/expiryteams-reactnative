9e5531fcc365899315ae393a4e81d33fc6b557f6
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-06-04 05:20:23 -0300

    Using shared category layout

--- src/Views/Category/Edit/index.tsx
+++ src/Views/Category/Edit/index.tsx
@@ -1,36 +1,16 @@
-import React, { useCallback, useState, useEffect, useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { showMessage } from 'react-native-flash-message';
-import Dialog from 'react-native-dialog';
 
-import Header from '@components/Header';
-import strings from '~/Locales';
-
-import { useTeam } from '~/Contexts/TeamContext';
+import { useTeam } from '@teams/Contexts/TeamContext';
 
 import {
 	getCategory,
 	updateCategory,
 	deleteCategory,
-} from '~/Functions/Categories';
+} from '@teams/Functions/Categories';
 
-import Loading from '@components/Loading';
-
-import {
-	ActionsButtonContainer,
-	ButtonPaper,
-	Icons,
-} from '~/Views/Product/Edit/styles';
-
-import {
-	Container,
-	Content,
-	PageTitleContainer,
-	InputTextContainer,
-	InputText,
-	InputTextTip,
-} from './styles';
+import EditCategory from '@views/Category/Edit';
 
 interface Props {
 	id: string;
@@ -48,87 +28,15 @@ const Edit: React.FC = () => {
 		return 'repositor';
 	}, [teamContext.roleInTeam]);
 
-	const [name, setName] = useState<string | undefined>(undefined);
-	const [errorName, setErrorName] = useState<string>('');
-
-	const [isLoading, setIsLoading] = useState<boolean>(true);
-	const [isMounted, setIsMounted] = useState(true);
-	const [deleteComponentVisible, setDeleteComponentVisible] = useState(false);
-
 	const routeParams = params as Props;
 
-	const handleDeleteCategory = useCallback(async () => {
-		try {
-			setIsLoading(true);
-			await deleteCategory({ category_id: routeParams.id });
-
-			reset({
-				routes: [
-					{
-						name: 'Home',
-					},
-					{ name: 'ListCategory' },
-				],
-			});
-		} catch (err) {
-			if (err instanceof Error)
-				showMessage({
-					message: err.message,
-					type: 'danger',
-				});
-		} finally {
-			setIsLoading(false);
-		}
-	}, [routeParams.id, reset]);
-
-	const loadData = useCallback(async () => {
-		if (!isMounted) return;
-		try {
-			setIsLoading(true);
-			const category = await getCategory({ category_id: routeParams.id });
-
-			setName(category.name);
-		} catch (err) {
-			if (err instanceof Error)
-				showMessage({
-					message: err.message,
-					type: 'danger',
-				});
-		} finally {
-			setIsLoading(false);
-		}
-	}, [isMounted, routeParams.id]);
-
-	useEffect(() => {
-		loadData();
-	}, [loadData]);
-
-	const onNameChange = useCallback(value => {
-		setErrorName('');
-		setName(value);
+	const getCategoryLocal = useCallback(async (id: string) => {
+		return getCategory({ category_id: id });
 	}, []);
 
-	const handleUpdate = useCallback(async () => {
-		if (!isMounted) return;
-		if (!name) {
-			setErrorName(strings.View_Category_Edit_ErrorEmtpyName);
-			return;
-		}
-
-		try {
-			setIsLoading(true);
-
-			await updateCategory({
-				category: {
-					id: routeParams.id,
-					name,
-				},
-			});
-
-			showMessage({
-				message: strings.View_Category_Edit_SuccessText,
-				type: 'info',
-			});
+	const updateCategoryLocal = useCallback(
+		async (category: ICategory) => {
+			await updateCategory({ category });
 
 			reset({
 				routes: [
@@ -138,89 +46,34 @@ const Edit: React.FC = () => {
 					},
 				],
 			});
-		} catch (err) {
-			if (err instanceof Error)
-				showMessage({
-					message: err.message,
-					type: 'danger',
-				});
-		} finally {
-			setIsLoading(false);
-		}
-	}, [isMounted, name, routeParams.id, reset]);
+		},
+		[reset]
+	);
 
-	const handleSwitchShowDeleteCategory = useCallback(() => {
-		setDeleteComponentVisible(!deleteComponentVisible);
-	}, [deleteComponentVisible]);
+	const deleteCategoryLocal = useCallback(
+		async (id: string) => {
+			await deleteCategory({ category_id: id });
 
-	useEffect(() => {
-		return setIsMounted(false);
-	}, []);
+			reset({
+				routes: [
+					{
+						name: 'Home',
+					},
+					{ name: 'ListCategory' },
+				],
+			});
+		},
+		[reset]
+	);
 
-	return isLoading ? (
-		<Loading />
-	) : (
-		<Container>
-			<PageTitleContainer>
-				<Header title={strings.View_Category_Edit_PageTitle} noDrawer />
-
-				<ActionsButtonContainer>
-					<ButtonPaper
-						icon={() => <Icons name="save-outline" size={22} />}
-						onPress={handleUpdate}
-					>
-						{strings.View_Category_Edit_ButtonSave}
-					</ButtonPaper>
-
-					{(userRole === 'manager' || userRole === 'supervisor') && (
-						<ButtonPaper
-							icon={() => (
-								<Icons name="trash-outline" size={22} />
-							)}
-							onPress={() => {
-								setDeleteComponentVisible(true);
-							}}
-						>
-							{strings.View_ProductDetails_Button_DeleteProduct}
-						</ButtonPaper>
-					)}
-				</ActionsButtonContainer>
-			</PageTitleContainer>
-
-			<Content>
-				<InputTextContainer hasError={!!errorName}>
-					<InputText
-						placeholder={
-							strings.View_Category_Edit_InputNamePlaceholder
-						}
-						value={name}
-						onChangeText={onNameChange}
-					/>
-				</InputTextContainer>
-				{!!errorName && <InputTextTip>{errorName}</InputTextTip>}
-			</Content>
-
-			<Dialog.Container
-				visible={deleteComponentVisible}
-				onBackdropPress={handleSwitchShowDeleteCategory}
-			>
-				<Dialog.Title>
-					{strings.View_Category_Edit_DeleteModal_Title}
-				</Dialog.Title>
-				<Dialog.Description>
-					{strings.View_Category_Edit_DeleteModal_Message}
-				</Dialog.Description>
-				<Dialog.Button
-					label={strings.View_Category_Edit_DeleteModal_Cancel}
-					onPress={handleSwitchShowDeleteCategory}
-				/>
-				<Dialog.Button
-					label={strings.View_Category_Edit_DeleteModal_Confirm}
-					color="red"
-					onPress={handleDeleteCategory}
-				/>
-			</Dialog.Container>
-		</Container>
+	return (
+		<EditCategory
+			id={routeParams.id}
+			getCategory={getCategoryLocal}
+			updateCategory={updateCategoryLocal}
+			deleteCategory={deleteCategoryLocal}
+			disableDelete={userRole === 'repositor'}
+		/>
 	);
 };
 
