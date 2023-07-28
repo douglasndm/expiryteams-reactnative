2e51c73b6b7ca038ec15b2e6840bb83010e98b6a
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-07-28 18:47:31 -0300

    API code errors strings now load automatically

--- src/Services/API/Errors.ts
+++ src/Services/API/Errors.ts
@@ -27,25 +27,25 @@ async function errorsHandler(error: any): Promise<void> {
 			}
 
 			code = Number(errorCode);
+			const result = Object.entries(strings);
+
+			const filted = result.filter(value =>
+				value[0].startsWith('API_Error_Code')
+			);
+
+			const errString = filted.find(
+				value => value[0] === `API_Error_Code${errorCode}`
+			);
+
 			switch (errorCode) {
-				case 1:
-					err = strings.API_Error_Code1;
-					break;
-				case 2:
-					err = strings.API_Error_Code2;
-					break;
 				case 3:
-					err = strings.API_Error_Code3;
 					reset({
 						routesNames: ['Logout'],
 					});
 					break;
-				case 4:
-					err = strings.API_Error_Code4;
-					break;
+
 				case 5:
 					// Subscription is not active
-					err = strings.API_Error_Code5;
 					/*
                     reset({
                         routeHandler: 'Routes',
@@ -53,64 +53,23 @@ async function errorsHandler(error: any): Promise<void> {
                     });
                     */
 					break;
-				case 6:
-					err = strings.API_Error_Code6;
-					break;
+
 				case 7:
 					// User was not found
-					err = strings.API_Error_Code7;
 					reset({
 						routesNames: ['Logout'],
 					});
 					break;
-				case 8:
-					err = strings.API_Error_Code8;
-					break;
-				case 9:
-					err = strings.API_Error_Code9;
-					break;
-				case 10:
-					err = strings.API_Error_Code10;
-					break;
-				case 11:
-					err = strings.API_Error_Code11;
-					break;
-				case 12:
-					err = strings.API_Error_Code12;
-					break;
-				case 13:
-					err = strings.API_Error_Code13;
-					break;
-				case 14:
-					err = strings.API_Error_Code14;
-					break;
-				case 15:
-					err = strings.API_Error_Code15;
-					break;
-				case 16:
-					err = strings.API_Error_Code16;
-					break;
+
 				case 17:
 					// User is not in team
 					// could be removed or manager deleted the team
-					err = strings.API_Error_Code17;
 					await clearSelectedteam();
 					reset({
 						routesNames: ['TeamList'],
 					});
 					break;
-				case 18:
-					err = strings.API_Error_Code18;
-					break;
-				case 19:
-					err = strings.API_Error_Code19;
-					break;
-				case 20:
-					err = strings.API_Error_Code20;
-					break;
-				case 21:
-					err = strings.API_Error_Code21;
-					break;
+
 				case 22:
 					// Device not allowed, login anywhere else
 					await destroySession();
@@ -118,22 +77,7 @@ async function errorsHandler(error: any): Promise<void> {
 						routeHandler: 'Auth',
 						routesNames: ['Login'],
 					});
-					err = strings.API_Error_Code22;
-					break;
-				case 23:
-					err = strings.API_Error_Code23;
-					break;
-				case 24:
-					err = strings.API_Error_Code24;
-					break;
-				case 25:
-					err = strings.API_Error_Code25;
-					break;
-				case 26:
-					err = strings.API_Error_Code26;
-					break;
-				case 27:
-					err = strings.API_Error_Code27;
+
 					break;
 
 				default:
@@ -141,6 +85,10 @@ async function errorsHandler(error: any): Promise<void> {
 						err = message;
 					}
 					break;
+			}
+
+			if (errString) {
+				err = errString[1];
 			}
 		} else if (error.response.data.message) {
 			err = error.response.data.message;
