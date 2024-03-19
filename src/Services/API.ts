dec6a18d82172fecb39e3301dfef98d88498bf53
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-03-19 20:43:17 -0300

    Adding auth token again

--- src/Services/API.ts
+++ src/Services/API.ts
@@ -1,6 +1,18 @@
+import auth from '@react-native-firebase/auth';
+
 import api from '@services/API';
 
 import errorsHandler from './API/Errors';
+
+api.interceptors.request.use(async config => {
+	const userToken = await auth().currentUser?.getIdToken();
+
+	if (userToken) {
+		config.headers.Authorization = `Bearer ${userToken}`;
+	}
+
+	return config;
+});
 
 api.interceptors.response.use(response => response, errorsHandler);
 
